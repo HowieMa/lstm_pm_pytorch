a690b8bcf6941bb699446002b9cf78b65be7c5e0
howie <mahaoyu_seu@163.com>
2018-10-12 00:29:49 +0800

    add draw

--- src/utils.py
+++ src/utils.py
@@ -206,5 +206,53 @@ def Tests_save_label_imgs(label_map, predict_heatmaps, step, imgs, temporal=13, 
 
 
 
+from PIL import Image
+from PIL import ImageDraw
+
+
+
+def draw_point(points, im):
+    """
+    draw key point on image
+    :param points: list 21 [ [x1,y1], ..., [x21,y21]  ]
+    :param im: PIL Image
+    :return:
+    """
+    i = 0
+    draw=ImageDraw.Draw(im)
+
+    for point in points:
+        x = point[1]
+        y = point[0]
+
+        if i==0:
+            rootx=x
+            rooty=y
+        if i==1 or i==5 or i==9 or i==13 or i==17:
+            prex=rootx
+            prey=rooty
+
+        if i >0 and i<=4:
+            draw.line((prex,prey,x,y),'red')
+            draw.ellipse((x-3, y-3, x+3, y+3), 'red', 'black')
+        if i >4 and i<=8:
+            draw.line((prex,prey,x,y),'yellow')
+            draw.ellipse((x-3, y-3, x+3, y+3), 'yellow', 'black')
+
+        if i >8 and i<=12:
+            draw.line((prex,prey,x,y),'green')
+            draw.ellipse((x-3, y-3, x+3, y+3), 'green', 'black')
+        if i >12 and i<=16:
+            draw.line((prex,prey,x,y),'blue')
+            draw.ellipse((x-3, y-3, x+3, y+3), 'blue', 'black')
+        if i >16 and i<=20:
+            draw.line((prex,prey,x,y),'purple')
+            draw.ellipse((x-3, y-3, x+3, y+3), 'purple', 'black')
+
+
+        prex=x
+        prey=y
+        i=i+1
+    return im
 
 
